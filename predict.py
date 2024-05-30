8c461ea1d4df292fda8c0757b0daddc2d4b8bf9d
Paul <fofr@users.noreply.github.com>
2024-05-30 10:26:27 +0000

    Run prepare template script

--- predict.py
+++ predict.py
@@ -1,22 +1,25 @@
+# An example of how to convert a given API workflow into its own Replicate model
+# Replace predict.py with this file when building your own workflow
+
 import os
-import shutil
-import tarfile
-import zipfile
 import mimetypes
+import json
+from PIL import Image, ExifTags
 from typing import List
 from cog import BasePredictor, Input, Path
 from comfyui import ComfyUI
 from cog_model_helpers import optimise_images
+from cog_model_helpers import seed as seed_helper
 
-os.environ["DOWNLOAD_LATEST_WEIGHTS_MANIFEST"] = "true"
-mimetypes.add_type("image/webp", ".webp")
 OUTPUT_DIR = "/tmp/outputs"
 INPUT_DIR = "/tmp/inputs"
 COMFYUI_TEMP_OUTPUT_DIR = "ComfyUI/temp"
 ALL_DIRECTORIES = [OUTPUT_DIR, INPUT_DIR, COMFYUI_TEMP_OUTPUT_DIR]
 
-with open("examples/api_workflows/sd15_txt2img.json", "r") as file:
-    EXAMPLE_WORKFLOW_JSON = file.read()
+mimetypes.add_type("image/webp", ".webp")
+
+# Save your example JSON to the same directory as predict.py
+api_json_file = "workflow_api.json"
 
 
 class Predictor(BasePredictor):
@@ -24,71 +27,96 @@ class Predictor(BasePredictor):
         self.comfyUI = ComfyUI("127.0.0.1:8188")
         self.comfyUI.start_server(OUTPUT_DIR, INPUT_DIR)
 
-    def handle_input_file(self, input_file: Path):
-        file_extension = os.path.splitext(input_file)[1].lower()
-        if file_extension == ".tar":
-            with tarfile.open(input_file, "r") as tar:
-                tar.extractall(INPUT_DIR)
-        elif file_extension == ".zip":
-            with zipfile.ZipFile(input_file, "r") as zip_ref:
-                zip_ref.extractall(INPUT_DIR)
-        elif file_extension in [".jpg", ".jpeg", ".png", ".webp"]:
-            shutil.copy(input_file, os.path.join(INPUT_DIR, f"input{file_extension}"))
-        else:
-            raise ValueError(f"Unsupported file type: {file_extension}")
+        # Give a list of weights filenames to download during setup
+        with open(api_json_file, "r") as file:
+            workflow = json.loads(file.read())
+        self.comfyUI.handle_weights(
+            workflow,
+            weights_to_download=[],
+        )
 
-        print("====================================")
-        print(f"Inputs uploaded to {INPUT_DIR}:")
-        self.comfyUI.get_files(INPUT_DIR)
-        print("====================================")
+    def handle_input_file(
+        self,
+        input_file: Path,
+        filename: str = "image.png",
+        check_orientation: bool = True,
+    ):
+        image = Image.open(input_file)
+
+        if check_orientation:
+            try:
+                for orientation in ExifTags.TAGS.keys():
+                    if ExifTags.TAGS[orientation] == "Orientation":
+                        break
+                exif = dict(image._getexif().items())
+
+                if exif[orientation] == 3:
+                    image = image.rotate(180, expand=True)
+                elif exif[orientation] == 6:
+                    image = image.rotate(270, expand=True)
+                elif exif[orientation] == 8:
+                    image = image.rotate(90, expand=True)
+            except (KeyError, AttributeError):
+                # EXIF data does not have orientation
+                # Do not rotate
+                pass
+
+        image.save(os.path.join(INPUT_DIR, filename))
+
+    # Update nodes in the JSON workflow to modify your workflow based on the given inputs
+    def update_workflow(self, workflow, **kwargs):
+        # Below is an example showing how to get the node you need and update the inputs
+
+        # positive_prompt = workflow["6"]["inputs"]
+        # positive_prompt["text"] = kwargs["prompt"]
+
+        # negative_prompt = workflow["7"]["inputs"]
+        # negative_prompt["text"] = f"nsfw, {kwargs['negative_prompt']}"
+
+        # sampler = workflow["3"]["inputs"]
+        # sampler["seed"] = kwargs["seed"]
+        pass
 
     def predict(
         self,
-        workflow_json: str = Input(
-            description="Your ComfyUI workflow as JSON. You must use the API version of your workflow. Get it from ComfyUI using ‘Save (API format)’. Instructions here: https://github.com/fofr/cog-comfyui",
+        prompt: str = Input(
             default="",
         ),
-        input_file: Path = Input(
-            description="Input image, tar or zip file. Read guidance on workflows and input files here: https://github.com/fofr/cog-comfyui. Alternatively, you can replace inputs with URLs in your JSON workflow and the model will download them.",
-            default=None,
+        negative_prompt: str = Input(
+            description="Things you do not want to see in your image",
+            default="",
         ),
-        return_temp_files: bool = Input(
-            description="Return any temporary files, such as preprocessed controlnet images. Useful for debugging.",
-            default=False,
+        image: Path = Input(
+            description="An input image",
+            default=None,
         ),
         output_format: str = optimise_images.predict_output_format(),
         output_quality: int = optimise_images.predict_output_quality(),
-        randomise_seeds: bool = Input(
-            description="Automatically randomise seeds (seed, noise_seed, rand_seed)",
-            default=True,
-        ),
-        force_reset_cache: bool = Input(
-            description="Force reset the ComfyUI cache before running the workflow. Useful for debugging.",
-            default=False,
-        ),
+        seed: int = seed_helper.predict_seed(),
     ) -> List[Path]:
         """Run a single prediction on the model"""
         self.comfyUI.cleanup(ALL_DIRECTORIES)
 
-        if input_file:
-            self.handle_input_file(input_file)
+        # Make sure to set the seeds in your workflow
+        seed = seed_helper.generate(seed)
 
-        wf = self.comfyUI.load_workflow(workflow_json or EXAMPLE_WORKFLOW_JSON)
+        if image:
+            self.handle_input_file(image)
 
+        with open(api_json_file, "r") as file:
+            workflow = json.loads(file.read())
+
+        self.update_workflow(
+            workflow,
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            seed=seed,
+        )
+
+        wf = self.comfyUI.load_workflow(workflow)
         self.comfyUI.connect()
-
-        if force_reset_cache or not randomise_seeds:
-            self.comfyUI.reset_execution_cache()
-
-        if randomise_seeds:
-            self.comfyUI.randomise_seeds(wf)
-
         self.comfyUI.run_workflow(wf)
 
-        output_directories = [OUTPUT_DIR]
-        if return_temp_files:
-            output_directories.append(COMFYUI_TEMP_OUTPUT_DIR)
-
         return optimise_images.optimise_image_files(
-            output_format, output_quality, self.comfyUI.get_files(output_directories)
+            output_format, output_quality, self.comfyUI.get_files(OUTPUT_DIR)
         )
